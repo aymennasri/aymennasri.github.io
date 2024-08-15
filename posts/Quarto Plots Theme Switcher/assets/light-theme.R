e2c971299272a7acb07ac1f05b5552703e2844e1
aymennasri <aymennasrii@proton.com>
2024-08-15 11:34:46 +0100

    Adding the quarto files.

---
+++ posts/Quarto Plots Theme Switcher/assets/light-theme.R
@@ -0,0 +1,25 @@
+light_theme <- function() {
+  ggthemes::theme_solarized_2() %+% 
+    theme(
+      plot.background = element_rect(fill = "#FFF1E5"),
+      panel.border = element_blank(),
+      axis.line = element_line(colour = "#586e75",
+                               linetype = 1),
+      axis.ticks = element_line(colour = "#586e75"),
+      axis.text = element_text(colour = "#002b36"),
+      legend.background = element_rect(fill = "#FFF1E5"))
+} 
+
+theme_set(light_theme())
+
+lightsvglite <- function(file, width, height) {
+  on.exit(reset_theme_settings())
+  theme_set(light_theme())
+  ggsave(
+    filename = file,
+    width = width,
+    height = height,
+    dev = "svg",
+    bg = "transparent"
+  )
+}
